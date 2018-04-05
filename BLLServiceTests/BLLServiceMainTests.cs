f8f9c18575f4e24eaeb4a070e722a9e3aa2007f0
Dzianis_Tarasevich <Dzianis_Tarasevich@EPBYGROW0335>
2018-04-05 17:41:41 +0300

    With test

---
+++ BLLServiceTests/BLLServiceMainTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BLL.EntitesDTO;
+using BLL.Interfaces;
+using GalaSoft.MvvmLight.Messaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using ViewModel.Helpers;
+using ViewModel.Models;
+using ViewModel.ViewModels.Appointments;
+
+namespace BLLServiceTests
+{
+    [TestClass]
+    public class BLLServiceMainTests
+    {
+        [TestMethod]
+        public void CanInitialize()
+        {
+            var mock = new Mock<IBLLServiceMain>();
+            mock.Setup(s => s.GetLocationById(1)).Returns(new LocationDTO
+            {
+                LocationId = 1,
+                Room = "testRoom"
+            });
+
+            AboutAppointmentWindowViewModel vm = new AboutAppointmentWindowViewModel(mock.Object);
+            Messenger.Default.Send(new OpenWindowMessage
+            {
+                Type = WindowType.None, Argument = "Load this appointment", Appointment = new AppointmentModel
+                {
+                    LocationId = 1
+                }
+            });
+
+            var locationId = vm.Location.LocationId;
+
+            Assert.AreEqual(locationId, 1);
+        }
+    }
+}
